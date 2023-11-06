1a6fceee83d24a2271075c71ca1127022783d1ca
Victor Vieira <victorbor2001@yahoo.com.br>
2023-11-06 02:35:52 -0300

    changed let to const

--- shared.js
+++ shared.js
@@ -1,7 +1,7 @@
 import { Board } from './classes/board.js'
 import { canvasId } from './consts.js'
 
-export let buttonObjs = []
+export const buttonObjs = []
 export const mainBoard = new Board()
 export const canvas = document.getElementById(canvasId)
 export const ctx = canvas.getContext('2d')
